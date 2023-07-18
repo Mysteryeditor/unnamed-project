915a92cf40d239f29c4b43c59e4efa6e6dfaff99
Mysteryeditor <sri027.cvb@gmail.com>
2023-07-18 21:54:14 +0530

    h

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -6,12 +6,10 @@ import { UsersService } from 'src/services/users.service';
 import { HttpClient } from '@angular/common/http';
 import Swal from 'sweetalert2';
 
-
-
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
   userId!: string | null;
@@ -24,11 +22,11 @@ export class TasksComponent implements OnInit {
     dueDate: '',
     userName: '',
     status: 'assigned',
-    assignedBy: ''
-  }
+    assignedBy: '',
+  };
   assignedDate!: string;
   todayDate = '08/20/2023';
-  adminOps: boolean = false
+  adminOps: boolean = false;
 
   taskData!: taskAssignment[];
   noTasks: boolean = false;
@@ -36,24 +34,32 @@ export class TasksComponent implements OnInit {
   sortingParam: string = '';
   sortingDirection: string = '';
   optionSelected: string = '';
+  taskDone: boolean = false;
 
   onSelectingOption(event: any) {
     this.optionSelected = event.target.value;
     console.log(this.optionSelected);
     if (this.optionSelected === 'uasc') {
       (this.sortingParam = 'userName'), (this.sortingDirection = 'asc');
-    }
-
-    else if (this.optionSelected === 'udesc') {
+    } else if (this.optionSelected === 'udesc') {
       (this.sortingParam = 'userName'), (this.sortingDirection = 'desc');
+    } else if (this.optionSelected === 'statassng') {
+      (this.sortingParam = 'status'), (this.sortingDirection = 'assigned');
+    } else if (this.optionSelected === 'statpending') {
+      (this.sortingParam = 'status'), (this.sortingDirection = 'Pending');
+    } else if (this.optionSelected === 'statdone') {
+      (this.sortingParam = 'status'), (this.sortingDirection = 'Done');
     }
-
   }
 
-  constructor(private actroute: ActivatedRoute, private taskservice: TasksServiceService, private userserv: UsersService, private http: HttpClient) { }
+  constructor(
+    private actroute: ActivatedRoute,
+    private taskservice: TasksServiceService,
+    private userserv: UsersService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
-
     this.userserv.getSessionInfo().subscribe((data) => {
       if (data.length > 0) {
         if (data[0].role === 'admin') {
@@ -61,18 +67,16 @@ export class TasksComponent implements OnInit {
           this.taskDetails.assignedBy = data[0].firstName;
         }
       }
-    })
-
+    });
 
     this.userId = this.actroute.snapshot.paramMap.get('id');
-    this.taskservice.getTasks(this.userId).subscribe(
-      (res) => {
-        this.taskData = res;
-        if (res.length < 1) {
-          this.noTasks = true;
-          // alert("No tasks found");
-        }
-      });
+    this.taskservice.getTasks(this.userId).subscribe((res) => {
+      this.taskData = res;
+      if (res.length < 1) {
+        this.noTasks = true;
+        // alert("No tasks found");
+      }
+    });
 
     const d = new Date();
     this.assignedDate = d.toUTCString();
@@ -83,21 +87,20 @@ export class TasksComponent implements OnInit {
           this.userNames.push(u.firstName);
         }
       }
-    })
+    });
   }
 
   getDaysLeft(assignedDate: string, dueDate: string) {
     const iniDate = new Date(assignedDate);
     const deadlin = new Date(dueDate);
     const milliSec = 24 * 60 * 60 * 1000;
-    return Math.round(Math.abs(Number(deadlin) - Number(iniDate)) / milliSec
-    )
+    return Math.round(Math.abs(Number(deadlin) - Number(iniDate)) / milliSec);
   }
   // task process
   setUserId() {
     this.userserv.getUserId(this.taskDetails.userName).subscribe((res) => {
       this.taskDetails.userid = res[0].id;
-    })
+    });
   }
 
   onSubmit() {
@@ -110,7 +113,7 @@ export class TasksComponent implements OnInit {
   deleteTask(taskId: number) {
     Swal.fire({
       title: 'Delete Confirmation',
-      text: "Do You Want To Remove This Record?",
+      text: 'Do You Want To Remove This Record?',
       showCancelButton: true,
       showLoaderOnConfirm: true,
     }).then((result) => {
@@ -123,12 +126,11 @@ export class TasksComponent implements OnInit {
         });
         this.ngOnInit();
       }
-    }
-    )
+    });
   }
 
   singleTaskData!: taskAssignment;
-  currentStatus!: string
+  currentStatus!: string;
   updateStatus(item: taskAssignment) {
     this.singleTaskData = item;
     this.currentStatus = this.singleTaskData.status;
@@ -138,6 +140,4 @@ export class TasksComponent implements OnInit {
     this.singleTaskData.status = this.currentStatus;
     this.taskservice.putSingleTask(this.singleTaskData);
   }
-
-
 }
